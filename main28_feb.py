cccc92de7b010a96460836199986cdf9d9100c17
adilsd55 <adilsd55@gmail.com>
2023-03-02 14:22:50 -0500

    love

--- main28_feb.py
+++ main28_feb.py
@@ -1,6 +1,15 @@
 def fun(**args):
-    print(args)
+    print(*args.values())
 
 data={'a':10,'b':20,'c':30}
 
 fun(**data)
+
+
+
+def Massage(**args):
+    print(*args.values())
+
+proposel={'i':'Adil','what':'Love','To':'You','Who':"Shikha"}
+
+Massage(**proposel)
---
+++ test_2.py
@@ -0,0 +1,6 @@
+def logo(x,y,z):
+    print(x,y,z)
+
+a=10;b=20;c=30
+
+logo(a,b,c)
